f9295a109be1efeaecac927dae3b16e79a60221e
Manty-S-Djusaem <104922578+Manty-S-Djusaem@users.noreply.github.com>
2023-03-04 13:20:27 +0600

    dz7

--- main.js
+++ main.js
@@ -3,30 +3,21 @@ const btn = document.querySelector('#btn')
 let count = 1
 
 
-const getData = async (url) =>{
+const getData = async (url) => {
     const responce = await fetch(`https://jsonplaceholder.typicode.com/posts/${count}`);
-    if (url === 'next'){
-        count ++
+    if (url === 'next') {
+        count++
     }
-     return  await responce.json()
+    return await responce.json()
         .then(data => {
             const div = document.createElement('div')
             div.setAttribute('class', 'card')
-                div.innerHTML = `
+            div.innerHTML = `
                 <img src="img/putin.webp" alt="">
                 <h2>${data.title}</h2>
-                <h3>${data.body}</h3>
-                
-               
-                 
-        `
-            // block.innerHTML = ''
+                <h3>${data.body}</h3>`
             block.append(div)
         })
-
-
-
-
 };
 
 btn.onclick = () => {
